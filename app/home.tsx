ed3b965be2a970b0aafeca2b438d2f71f9677aca
Murad Qumizakis <muradqumizakis@gmail.com>
2023-08-03 03:20:22 -0500

    comitting

--- app/home.tsx
+++ app/home.tsx
@@ -46,36 +46,42 @@ export default function Home({ session }: any) {
           </h3>
           <CreateGame setGame={setGames} session={session} />
         </div>
-        <div className="flex flex-col justify-center overflow-auto">
-          <section className="flex flex-col justify-center items-center mt-2">
-            <h3 className="flex justify-center text-center text-xl mt-10">
-              Play existing games
-            </h3>
+        {session ? (
+          <div className="flex flex-col justify-center overflow-auto">
+            <section className="flex flex-col justify-center items-center mt-2">
+              <h3 className="flex justify-center text-center text-xl mt-10">
+                Play existing games
+              </h3>
 
-            <div className="flex flex-col justify-evenly gap-4 w-96 p-4">
-              {loading && (
-                <div className="flex items-center justify-center">
-                  Loading existing games...
-                </div>
-              )}
-              {games.length === 0 && !loading && (
-                <div className="flex items-center justify-center">
-                  No games found, create one!
-                </div>
-              )}
-              {games.map((game: Game) => (
-                <Game
-                  key={game.id}
-                  id={game.id}
-                  gameName={game.name}
-                  onDelete={() =>
-                    setGames(games.filter((g) => g.id !== game.id))
-                  }
-                />
-              ))}
-            </div>
-          </section>
-        </div>
+              <div className="flex flex-col justify-evenly gap-4 w-96 p-4">
+                {loading && (
+                  <div className="flex items-center justify-center">
+                    Loading existing games...
+                  </div>
+                )}
+                {games.length === 0 && !loading && (
+                  <div className="flex items-center justify-center">
+                    No games found, create one!
+                  </div>
+                )}
+                {games.map((game: Game) => (
+                  <Game
+                    key={game.id}
+                    id={game.id}
+                    gameName={game.name}
+                    onDelete={() =>
+                      setGames(games.filter((g) => g.id !== game.id))
+                    }
+                  />
+                ))}
+              </div>
+            </section>
+          </div>
+        ) : (
+          <h3 className="justify-center items-center">
+            Please Sign In to view games
+          </h3>
+        )}
       </div>
     </div>
   );
